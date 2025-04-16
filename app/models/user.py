0443655644474df8bab9a613caee1e268f4fc4a9
Facundo Pyznar <fpyznar@mail.austral.edu.ar>
2025-04-16 17:01:15 -0300

    Cambio de varchar

--- app/models/user.py
+++ app/models/user.py
@@ -7,7 +7,7 @@ class User(db.Model, UserMixin):
 
     IDUser = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(512), nullable=False)
 
     def __init__(self, username, password):
         self.username = username
